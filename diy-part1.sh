f0d5b3cc9c5dee08b07ca6a0d23488705d27a9f4
Michaol <michaol@gmail.com>
2024-12-08 14:21:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,10 +9,3 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-
-# drop mosdns and v2ray-geodata packages that come with the source
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
-
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
